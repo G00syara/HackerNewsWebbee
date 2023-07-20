445e4d221f93c9e50211c5879234dd5e8a8ccfd4
Александр Угаров <augarov@web-bee.ru>
2023-07-20 22:20:20 +0300

    Создал шаблон + добавил все библиотеки

---
+++ hackernews/src/components/HackerNewsForm/HackerNewsForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HackerNewsForm = () => {
+  return <div></div>;
+};
+
+export default HackerNewsForm;
---
+++ hackernews/src/components/HackerNewsList/HacketNewsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HacketNewsList = () => {
+  return <div></div>;
+};
+
+export default HacketNewsList;
